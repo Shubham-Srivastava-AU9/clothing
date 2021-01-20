75aca0f9ec810f7ec1b35b9cfefd248d9afe8770
Shubham-Srivastava-AU9 <cashivamsms@gmail.com>
2021-01-21 03:28:18 +0530

    adding

---
+++ build/precache-manifest.647904128756d39ba579ecc4ba724d5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a59484b8018d29ac79a8",
+    "url": "/static/css/main.bf512ed5.chunk.css"
+  },
+  {
+    "revision": "a59484b8018d29ac79a8",
+    "url": "/static/js/main.f579ad67.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b3edaa289bbf8d4a2992",
+    "url": "/static/js/2.ac728db7.chunk.js"
+  },
+  {
+    "revision": "9426d59aa7ab270cecc19f17db99984c",
+    "url": "/static/media/newLogo.9426d59a.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "7383ca60f038978f2d0627c139f905a6",
+    "url": "/index.html"
+  }
+];
